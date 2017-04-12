fffe84732e7419da72f64d1f4b3d57400b39e8ed
Andrei-Khadziukou <khadziukou_andrei@bk.ru>
2017-04-12 14:41:42 +0300

    Initial commit

---
+++ src/main/java/com/epam/petclinic/eureka/PetClinicEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.epam.petclinic.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicEurekaApplication.class, args);
+	}
+}
