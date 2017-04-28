7c6da1a1ce61bd71527bd7574679625e94d3e0c7
Andrei-Khadziukou <Andrei_Khadziukou@epam.com>
2017-04-28 15:46:53 +0300

    Eureka configuration was corrected.
    Refactoring: tabs were replaced to spaces; annotation clean up for Spring Boot application
    Spring Cloud project dependency version was changed.

--- src/main/java/com/epam/petclinic/eureka/PetClinicEurekaApplication.java
+++ src/main/java/com/epam/petclinic/eureka/PetClinicEurekaApplication.java
@@ -2,12 +2,10 @@ package com.epam.petclinic.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableDiscoveryClient
 public class PetClinicEurekaApplication {
 
     public static void main(String[] args) {
